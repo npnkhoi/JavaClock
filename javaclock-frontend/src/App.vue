42e8db21380633be0c5e1431cdd3f25c9c23b7bb
npnkhoi <npnkhoi@gmail.com>
2020-10-01 13:51:08 +0700

    create clock (current time)

---
+++ javaclock-frontend/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Clock/> 
+</template>
+
+<script>
+import Clock from './components/Clock';
+
+export default {
+  name: 'App',
+  components: {
+    Clock
+  }
+}
+</script>
---
+++ javaclock-frontend/src/components/Clock.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container w-full m-0 bg-gray-400">
+    <div class="time text-red-600"> {{ currentTime }} </div>
+  </div>
+  
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+  data () {
+    return {
+      currentTime: "Loading clock"
+    }
+  },
+  mounted: function () {
+    setInterval(() => {
+      this.currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+    }, 1000);
+  }
+}
+</script>
+
+<style>
+
+</style>
